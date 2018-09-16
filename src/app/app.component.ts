b6c1c6765a6ad7f3226a29b914f88dd99d28c7fc
derrick12 <derrickwaweru@gmail.com>
2018-09-16 12:34:03 +0300

    add a quote array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
+
 export class AppComponent {
-  title = 'Quotes';
+  quotes: string[];
+
+  constructor () {
+    // tslint:disable-next-line:max-line-length
+    this.quotes = ["Dont cry because its over, smile because it happened", "I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best."];
+  }
 }
