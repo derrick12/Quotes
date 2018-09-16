5f999fbbcc2cb8bfcd6147d883ece0a228bdd035
derrick12 <derrickwaweru@gmail.com>
2018-09-16 13:55:54 +0300

    create quote details component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,6 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  quotes: string[];
 
-  constructor () {
-    // tslint:disable-next-line:max-line-length
-    this.quotes = ["Dont cry because its over, smile because it happened", "I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best."];
-  }
+  constructor () { }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {Quote} from '../quote';
 
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
+
 export class QuoteComponent implements OnInit {
+
+  quotes = [
+    new Quote(1, 'Dont cry because its over smile because it happened'),
+    new Quote(2, 'Be yourself everyone else is already taken'),
+    new Quote(2, 'Be yourself everyone else is already taken')
+  ];
+
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
