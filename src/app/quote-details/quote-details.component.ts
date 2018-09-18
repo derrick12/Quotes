92399a94dbb751115261b0e739ba26e246f92a9e
derrick12 <derrickwaweru@gmail.com>
2018-09-18 15:20:21 +0300

    add vote functionality

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -4,16 +4,49 @@ import {Quote} from '../quote';
 @Component({
   selector: 'app-quote-details',
   templateUrl: './quote-details.component.html',
-  styleUrls: ['./quote-details.component.css']
+  styleUrls: ['./quote-details.component.css'],
 })
 export class QuoteDetailsComponent implements OnInit {
 
   @Input() quote: Quote;
+
+  @Input() voteCount = 0;
+  @Input() myVote = 0;
+
   @Output() isComplete = new EventEmitter<boolean>();
+
+  // tslint:disable-next-line:no-output-rename
+  @Output('vote') change = new EventEmitter();
+
+
 
   quoteDelete(complete: boolean) {
     this.isComplete.emit(complete);
   }
+
+  upVote() {
+    if (this.myVote === 1) {
+        return;
+    }
+
+    this.myVote++;
+    this.emitEvent();
+}
+
+downVote() {
+    if (this.myVote === -1) {
+        return;
+    }
+
+    this.myVote--;
+    this.emitEvent();
+}
+
+  emitEvent() {
+      this.change.emit({myVote: this.myVote});
+  }
+
+
   constructor() { }
 
   ngOnInit() {
